fe63df5b183fa792b8456653090b17f48c500073
RenCamp96 <rcampuz@hotmail.com>
2018-03-20 16:34:26 -0600

    AAAAAAAA

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 /*
  *
  * Cinema Expandido WEB
- * Pixel Array (13 de febrero 2018)
- * Adrian Santuario
+ * Tarea Pixel Array (19 de marzo 2018)
+ * Renata Campuzano
  * 
  *
  * URL: https://rencamp96.github.io/CEW_pixelarrayalo/.
@@ -31,8 +31,8 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function preload(){
-   video = createVideo("assets/videos/gato.mov");
+function preload() {
+  video = createVideo("assets/videos/colores.mov");
 }
 
 function setup() {
@@ -43,7 +43,7 @@ function setup() {
 function draw() {
   background(250);
   drawVideoNose();
-  toggleVideo();
+  //toggleVideo();
 }
 
 
@@ -62,84 +62,30 @@ function initilizeVideo() {
 
 }
 
-function drawVideo() {
-  var correctionX = (windowWidth / 2) - video.width / 2;
-  var correctionY = (windowHeight / 2) - video.height / 2;
-
-  video.loadPixels();
-
-  for (var y = 0; y < video.height; y++) {
-
-    for (var x = 0; x < video.width; x++) {
-        var index = (x + (y * video.width))*4;
-        
-        video.pixels[index]  = video.pixels[index +1];// Aquí tengo los rojos
-        video.pixels[index + 1]  = video.pixels[index + 2] ;
-        video.pixels[index + 2]  = video.pixels[index + 1] ;
-        video.pixels[index + 3]  = video.pixels[index + 3] ;
-    }
-  }
-
-  video.updatePixels();
-
-  image(video, correctionX, correctionY);
-}
-
-function drawVideoLuz() {
-  var correctionX = (windowWidth / 2) - video.width / 2;
-  var correctionY = (windowHeight / 2) - video.height / 2;
-
-  video.loadPixels();
-
-  for (var y = 0; y < video.height; y++) {
-
-    for (var x = 0; x < video.width; x++) {
-        var index = (x + (y * video.width))*4;
-        
-        if(index%130==0){
-        video.pixels[index]  = video.pixels[index +1];// Aquí tengo los rojos
-        video.pixels[index + 1]  = video.pixels[index + 2] ;
-        video.pixels[index + 2]  = video.pixels[index + 1] ;
-        video.pixels[index + 3]  = video.pixels[index + 3] ;
-        }
-    }
-  }
-
-  video.updatePixels();
-
-  image(video, correctionX, correctionY);
-}
 
 function drawVideoNose() {
   var correctionX = (windowWidth / 2) - video.width / 2;
   var correctionY = (windowHeight / 2) - video.height / 2;
 
   video.loadPixels();
-  var stepSize = round(map(mouseX,0,windowWidth,8,60));
-  
-  for (var y = 0; y < video.height; y+=stepSize) {
+  var stepSize = 25;
+  var col = round(map(mouseX, 0, windowHeight, -100, 20));
+  background(0);
 
-    for (var x = 0; x < video.width; x+=stepSize) {
-        var index = (x + y * video.width)*4;
-        
-        var darkness=(255 - video.pixels[index])/255;
-        var radio = stepSize*darkness;
-        noStroke();
-        fill(video.pixels[index],video.pixels[index+1],video.pixels[index+2]);
-        ellipse(x,y,radio,radio);
-        
+  for (var y = 0; y < video.height; y += stepSize) {
+
+    for (var x = 0; x < video.width; x += stepSize) {
+      var index = (x + y * video.width) * 4;
+
+      var darkness = (255 - video.pixels[index]) / 255;
+      var radio = stepSize * darkness;
+      noStroke();
+      fill(video.pixels[index] + col, video.pixels[index + 1] + col, video.pixels[index + 2] + col);
+      if (mouseY <= windowHeight / 2) {
+        rect(x, y, radio, radio);;
+      } else {
+        ellipse(x, y, radio, radio);;
+      }
     }
-  }
-
-  //video.updatePixels();
-
-  //image(video, correctionX, correctionY);
-}
-
-function toggleVideo(){
-  if(mouseY<=windowHeight/2){
-    video.loop();
-  }else{
-    video.pause();
   }
 }
